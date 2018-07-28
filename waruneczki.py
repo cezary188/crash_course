c519b8d4d17c1ea08d4881a82b9b8c3f7b9c2f51
cezary188 <cezary188@gmail.com>
2018-07-28 18:26:41 +0100

    punktowanie za wpisanie koloru aliena

---
+++ waruneczki.py
@@ -0,0 +1,17 @@
+
+print("Jakiego koloru byl alien ?")
+alien_color = input()
+dostepne = ['green', 'red', 'yellow']
+points = 0
+if alien_color == 'green':
+    points = + 5
+    print("Brawo twoja odpowiedz jest punktowana:\n")
+    print(points)
+elif alien_color == 'red':
+    points = + 3
+    print("Brawo twoja odpowiedz jest punktowana:\n")
+    print(points)
+elif alien_color == 'yellow':
+    points = + 1
+    print("Brawo twoja odpowiedz jest punktowana:\n")
+    print(points)
