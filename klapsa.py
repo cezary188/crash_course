44f5f864c311fc47bf86f6bab6d532f656c19896
cezary188 <cezary188@gmail.com>
2018-07-29 19:56:22 +0100

    dodano komentarz jak python sprawdza czy lista jest pusta

--- klapsa.py
+++ klapsa.py
@@ -1,6 +1,6 @@
 usernames = ['Mariusz', 'Sanczo', 'Michal', 'Sandra']
 
-if usernames:
+if usernames:											#w tym warunku po samym wpisaniu NAZWY listy python sam sprawdza czy jest  pusta jak tak to przechodzi do korespondujacego else na dole
 	for powitanie in usernames:
 		if powitanie =='Sandra':
 			print("Witaj Zlotko, chcesz klapsa ?")
